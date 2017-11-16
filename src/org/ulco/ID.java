f350c92794328fcd8382d27dcdeedcdd555319ba
BuildTools <unconfigured@null.spigotmc.org>
2017-11-16 14:51:21 +0100

    Refactoring - Quatrième tâche Transformation de la classe ID en singleton afin de supprimer la variable publique statique ID

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,14 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private int ID = 0;
+    private static ID instance = new ID();
+
+    private ID() {}
+
+    public static ID getInstance() { return instance; }
+
+    public int getNewId() {
+        return ID++;
+    }
 }
