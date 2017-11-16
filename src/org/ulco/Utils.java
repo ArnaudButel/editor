23a1ccd8b2eb7b6e9b01613c42a462b78e9d202d
BuildTools <unconfigured@null.spigotmc.org>
2017-11-16 15:29:47 +0100

    Refactoring - Cinquième tâche Externaliser les méthodes select dans une classe Utils

---
+++ src/org/ulco/Utils.java
@@ -0,0 +1,27 @@
+package org.ulco;
+
+import java.util.Vector;
+
+public class Utils {
+
+    public static GraphicsObjects select(Point pt, double distance, Layer layer) {
+        GraphicsObjects list = new GraphicsObjects();
+
+        for (GraphicsObject object : layer.getM_list()) {
+            if (object.isClosed(pt, distance)) {
+                list.add(object);
+            }
+        }
+        return list;
+    }
+
+
+    public static GraphicsObjects select(Point pt, double distance, Document doc) {
+        GraphicsObjects list = new GraphicsObjects();
+
+        for (Layer layer : doc.getM_layers()) {
+            list.addAll(Utils.select(pt, distance, layer));
+        }
+        return list;
+    }
+}
